ed2302dbf13046a1e2924576c02f52582f119cdf
Vighnesh Mishra <vighnesh97531@gmail.com>
2023-07-08 00:17:18 +0530

    [Added] eve url

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express")
 const app = express();
 const ticketRouter = require("./routes/ticket")
 const preRouter = require("./routes/preolym")
+const eveRouter = require("./routes/eve")
 
 app.get('/' , (req,res) => {
     res.send({"v" : "m"});
 });
 app.use("/ticket", ticketRouter);
 app.use("/preolym", preRouter);
+app.use("/eve", eveRouter);
 
 app.listen(3000);
