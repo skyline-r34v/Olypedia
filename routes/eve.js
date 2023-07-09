de7f21441d9873501d32e6caf32c4fe7f3ededa6
sanika-wani <wanisanika2004@gmail.com>
2023-07-09 13:39:47 +0530

    [Added]subsidiaries of /eve

--- routes/eve.js
+++ routes/eve.js
@@ -5,4 +5,29 @@ router.get('/', (req,res) =>
 {
       res.send("Event Management")
 });
+router.get('/sortgame', (req,res) =>
+{
+      res.send("sort by game")
+});
+router.get('/sortcountry', (req,res) =>
+{
+      res.send("sort by country")
+});
+
+router.get('/venue', (req,res) =>
+{
+      res.send("Venue page")
+});
+router.get('/venue/day1', (req,res) =>
+{
+      res.send("Today's Venue")
+});
+router.get('/venue/day2', (req,res) =>
+{
+      res.send("Tomorrow's Venue")
+});
+router.get('/venue/day3', (req,res) =>
+{
+      res.send("Day After Tomorrow's Venue")
+});
 module.exports = router;
