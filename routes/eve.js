6db63abb3481f666fa0ec69eba67e059d2d0fc6b
Vighnesh Mishra <vighnesh97531@gmail.com>
2023-07-11 21:29:48 +0530

    [Modificatio]

--- routes/eve.js
+++ routes/eve.js
@@ -5,29 +5,29 @@ router.get('/', (req,res) =>
 {
       res.send("Event Management")
 });
-router.get('/sortGame', (req,res) =>
+router.get('/sortgame', (req,res) =>
 {
       res.send("sort by game")
 });
-router.get('/sortCountry', (req,res) =>
+router.get('/sortcountry', (req,res) =>
 {
       res.send("sort by country")
 });
 router.get('/venue',(req,res) => {
       res.send("Venue Page")
 })
-//days -->
-router.get("/venue/:myparam", async (req, res) => {
+//sort by country -->
+router.get("/sortcountry/:myparam", async (req, res) => {
       const myParam = req.params.myparam;
   
-      if (myParam === "day1") {
-          res.send("Venue Day 1");
+      if (myParam === "d1") {
+          res.send("Venue (C) Day 1");
       } 
-      else if (myParam === "day2") {
-          res.send("Venue Day 2");
+      else if (myParam === "d2") {
+          res.send("Venue (C) Day 2");
       } 
-      else if (myParam === "day3") {
-            res.send("Venue Day 3");
+      else if (myParam === "d3") {
+            res.send("Venue (C) Day 3");
         }
       else {
           // Handle other cases if necessary
@@ -35,6 +35,26 @@ router.get("/venue/:myparam", async (req, res) => {
           res.status(404);
       }
   });
+  //sort by game
+  router.get("/sortgame/:myparam", async (req, res) => {
+      const myParam = req.params.myparam;
+  
+      if (myParam === "d1") {
+          res.send("Venue (G) Day 1");
+      } 
+      else if (myParam === "d2") {
+          res.send("Venue (G) Day 2");
+      } 
+      else if (myParam === "d3") {
+            res.send("Venue (G) Day 3");
+        }
+      else {
+          // Handle other cases if necessary
+          res.send("Invalid parameter");
+          res.status(404);
+      }
+  });
+
   //activity -->
   router.get('/activity',(req,res) => {
       res.send("Activity Page")
