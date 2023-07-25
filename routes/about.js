515d873fa53fbee1f9fb6f28cd9da14e32dd90da
Vighnesh Mishra <vighnesh97531@gmail.com>
2023-07-25 20:04:03 +0530

    [Rendering]

--- routes/about.js
+++ routes/about.js
@@ -2,6 +2,6 @@ const express = require("express")
 const router = express.Router();
 const showdb = require('../connection')
 
-router.get('/', showdb) // connection establishment
+router.get('/', showdb) // connection establishment // test connection reference
  
 module.exports = router;
--- routes/preolym.js
+++ routes/preolym.js
@@ -10,14 +10,14 @@ router.get('/london', (req,res) =>
 {
       res.sendFile(path.join(__dirname,"../public/london2012.html"))
 });
-router.get('/brazil', (req,res) =>
+router.get('/rio', (req,res) =>
 {
-      res.send("Brazil 2016")
+      res.sendFile(path.join(__dirname,"../public/rio2016.html"))
 });
 
 router.get('/tokyo', (req,res) =>
 {
-      res.send("Tokyo 2020")
+      res.sendFile(path.join(__dirname,"../public/tokyo2020.html"))
 });
 router.use(express.static('public'))
 module.exports = router;
